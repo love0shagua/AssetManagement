ebcce07bd530a3bf8f37dc070482f85116a6d518
zy1110 <zhengyao7788@126.com>
2016-04-20 19:18:46 +0800

    111

--- myasset/src/com/action/AddUserAction.java
+++ myasset/src/com/action/AddUserAction.java
@@ -4,25 +4,42 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.service.IUserService;
 import com.entity.Userinformation;
 public class AddUserAction extends ActionSupport {
-	private Userinformation userinformation;
+	
 	private IUserService userService;
-	public Userinformation getUserinformation() {
-		return userinformation;
-	}
-
-	public void setUserinformation(Userinformation userinformation) {
-		this.userinformation = userinformation;
-	}
+	private String username;
+	
 	public IUserService getUserService() {
 		return userService;
 	}
 
+
+
 	public void setUserService(IUserService userService) {
 		this.userService = userService;
 	}
-	public String addUser(){
-		
-		userService.addUser(userinformation);
-		return "add";
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+	
+
+
+	
+	public String addUserInfo(){
+		Userinformation userinformation = new Userinformation();
+		userinformation.setUsername(username);
+	    userService.addUser(userinformation);
+		return SUCCESS;
 	}
 }
