5dab6513b8598acf020231433d6ba54dad99a643
love0shagua <zhengyao7788@126.com>
2016-04-22 15:36:15 +0800

    Update IAssetClassDao.java

--- myasset/src/com/dao/IAssetClassDao.java
+++ myasset/src/com/dao/IAssetClassDao.java
@@ -2,6 +2,12 @@ package com.dao;
 
 import java.util.List;
 
+import com.entity.Assetclass;
+
 public interface IAssetClassDao {
-	
+	public boolean addAssetClass(Object obj);
+	public boolean removeAssetClass(Object obj);
+	public boolean updateAssetClass(Object obj);
+	public Assetclass findAssetClass(int id);
+	public List findAllAssetClass();
 }
