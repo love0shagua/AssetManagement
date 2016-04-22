5e5854d068726872d8e15eac222f9970d9723788
jwnever <jw_never@163.com>
2016-04-22 15:19:20 +0800

    Update AssetClassService.java

--- myasset/src/com/service/impl/AssetClassService.java
+++ myasset/src/com/service/impl/AssetClassService.java
@@ -8,19 +8,5 @@ import com.service.IAssetClassService;
 import com.service.IUserService;
 
 public class AssetClassService implements IAssetClassService {
-	private IUserDao userDao;
-
-	public IUserDao getUserDao() {
-		return userDao;
-	}
-
-	public void setUserDao(IUserDao userDao) {
-		this.userDao = userDao;
-	}
-	
-	public boolean AddAssetClass(Integer userid,String userpwd) {
-	
-		return true;
-	}
 
 }
