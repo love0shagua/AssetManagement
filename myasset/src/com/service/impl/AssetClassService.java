f4133be2b492fd91b5be15ccd98dcd3f3db0290d
love0shagua <zhengyao7788@126.com>
2016-04-22 15:38:22 +0800

    Update AssetClassService.java

--- myasset/src/com/service/impl/AssetClassService.java
+++ myasset/src/com/service/impl/AssetClassService.java
@@ -2,11 +2,48 @@ package com.service.impl;
 
 import java.util.List;
 
+import com.dao.IAssetClassDao;
 import com.dao.IUserDao;
+import com.entity.Assetclass;
 import com.entity.Userinformation;
 import com.service.IAssetClassService;
 import com.service.IUserService;
 
 public class AssetClassService implements IAssetClassService {
+	private IAssetClassDao assetClassDao;
+
+	
+	
+	public IAssetClassDao getAssetClassDao() {
+		return assetClassDao;
+	}
+
+
+
+	public void setAssetClassDao(IAssetClassDao assetClassDao) {
+		this.assetClassDao = assetClassDao;
+	}
+
+
+
+	public boolean addAssetClass(Assetclass assetclass) {
+		return assetClassDao.addAssetClass(assetclass);
+	}
+	
+	public boolean removeAssetClass(Assetclass assetclass) {
+		return assetClassDao.removeAssetClass(assetclass);
+	}
+	
+	public boolean updateAssetClass(Assetclass assetclass){
+		return assetClassDao.updateAssetClass(assetclass);
+	}
+	
+	public List findAllAssetClass(){
+		return assetClassDao.findAllAssetClass();
+	}
+	
+	public Assetclass findAssetClass(int id){
+		return assetClassDao.findAssetClass(id);
+	}
 
 }
