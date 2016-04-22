c1c3f0c331091aa473e3592149ce49e2dba0d19a
love0shagua <zhengyao7788@126.com>
2016-04-22 15:37:46 +0800

    Update IAssetClassService.java

--- myasset/src/com/service/IAssetClassService.java
+++ myasset/src/com/service/IAssetClassService.java
@@ -1,6 +1,14 @@
 package com.service;
 
+import java.util.List;
+
+import com.entity.Assetclass;
 import com.entity.Userinformation;
 
 public interface IAssetClassService {
+	public boolean addAssetClass(Assetclass assetclass);
+	public boolean removeAssetClass(Assetclass assetclass);
+	public boolean updateAssetClass(Assetclass assetclass);
+	public Assetclass findAssetClass(int id);
+	public List findAllAssetClass();
 }
