d6659f40bb5de8daaf7eccb8212f880881acac6c
love0shagua <zhengyao7788@126.com>
2016-04-22 15:33:36 +0800

    Update AssetClassDao.java

--- myasset/src/com/dao/impl/AssetClassDao.java
+++ myasset/src/com/dao/impl/AssetClassDao.java
@@ -8,6 +8,7 @@ import org.hibernate.Transaction;
 
 import com.dao.IAssetClassDao;
 import com.dao.IUserDao;
+import com.entity.Assetclass;
 
 public class AssetClassDao extends HibernateDaoSupport implements IAssetClassDao {
 	private SessionFactory sessionFactory;
@@ -20,5 +21,48 @@ public class AssetClassDao extends HibernateDaoSupport implements IAssetClassDao
 
 		this.sessionFactory = sessionFactory;//get/set������springע��ʱʹ��
 	}
+	
+	public boolean addAssetClass(Object obj){
+		
+		try {
+			this.getHibernateTemplate().save(obj);  
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+	
+	public boolean removeAssetClass(Object obj){
+		
+		try {
+            this.getHibernateTemplate().delete(obj); 
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
+	
+	public boolean updateAssetClass(Object obj){
+		try {
+            this.getHibernateTemplate().update(obj); 
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public List findAllAssetClass(){
+		try {
+			return this.getHibernateTemplate().find("from Assetclass");  			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	public Assetclass findAssetClass(int id){
+		return (Assetclass)this.getHibernateTemplate().get(Assetclass.class, id);
+	}
+
 
 }
